74b5285a0f2a83d14a4cc2e6b2d78af971c3c40f
siddharth jain <siddharthjn93@gmail.com>
2017-05-04 15:59:54 +0530

    "login to pwd to landing screen"

--- app/controllers/getin/emailCtrl.js
+++ app/controllers/getin/emailCtrl.js
@@ -1,17 +1,21 @@
-nevApp.controller("emailCtrl", function ($state, $scope, $httpshooter) {
-    console.log('inside email controller');
-    $scope.emailId = null;
-    $scope.submitEmail = function(){
-        $httpshooter.queue({
-            method: 'POST',
-        url: api.email,
-            data: {
-                email: $scope.emailId
-            }
-        }).then(function(data){
-               if(data.message){
-                   $state.go('getin.pwd');
-               }
-        })
-    }
+nevApp.controller("emailCtrl", function($state, $scope, $httpshooter,$localstorage) {
+  console.log('inside email controller');
+  $scope.emailId = null;
+  $scope.submitEmail = function() {
+    $httpshooter.queue({
+      method: 'POST',
+      url: api.email,
+      data: {
+        email: $scope.emailId
+      }
+    }).then(function(data) {
+      if (data.message) {
+        data=data.data;
+        if (data.state == 'goToPwd') {
+          $localstorage.userData=data;
+          $state.go('getin.pwd');
+        }
+      }
+    })
+  }
 });
--- app/controllers/getin/pwdCtrl.js
+++ app/controllers/getin/pwdCtrl.js
@@ -1,4 +1,4 @@
-nevApp.controller("pwdCtrl",function($state,$scope,$httpshooter){
+nevApp.controller("pwdCtrl",function($state,$scope,$httpshooter,$localstorage){
     console.log('inside epwd controller')
     $scope.pwdVal=null;
     $scope.submitPwd = function(){
@@ -6,12 +6,13 @@ nevApp.controller("pwdCtrl",function($state,$scope,$httpshooter){
             method: 'POST',
             url: api.pwd,
             data: {
+                email:$localstorage.userData.
                 pwd: $scope.pwd
             }
         }).then(function(data){
                if(data.message){
                    $state.go('getin.pwdVal');
-               } 
+               }
         })
     };
 });
