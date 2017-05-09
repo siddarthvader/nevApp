0f75699c715a4b217e88d2d85d4d0264d5c19875
siddharth jain <siddharthjn93@gmail.com>
2017-05-10 00:11:29 +0530

    token recieved

--- app/run.js
+++ app/run.js
@@ -1,6 +1,6 @@
 nevApp.run(function ($state, $timeout, $rootScope, $window, $localStorage) {
     console.info('iadhr');
-    $state.go('landing');
+    $state.go('getin');
     $rootScope.$state = $state;
     $window.$ls = $localStorage;
     $window.$state = $state;
