06a8d43a9ae0f73e9562f67e8440d660b874c43c
siddharth jain <siddharthjn93@gmail.com>
2017-05-22 08:32:58 +0530

    seasonality screen almost done

--- app/controllers/landing/seasonCtrl.js
+++ app/controllers/landing/seasonCtrl.js
@@ -1,11 +1,15 @@
 nevApp.controller('seasonCtrl', function ($state, $scope, $httpshooter, $localStorage, $rootScope, $sessionStorage) {
     var season = this;
-    season.productType = 'CUR';
-
+    $scope.searchProductType = 'CUR';
+    $scope.Object=Object;
     $scope.currencyData;
     $scope.currentYear = moment().year() - 1;
     $scope.years = [];
-    $scope.types=['long','short'];
+    $scope.types = ['long', 'short'];
+
+    //form vars
+    $scope.conWeeks = true
+
     for (i = 1; i <= 6; i++) {
         $scope.years.push($scope.currentYear - i * 5);
     }
@@ -18,7 +22,18 @@ nevApp.controller('seasonCtrl', function ($state, $scope, $httpshooter, $localSt
         // var BASE_URL = 'https://query.yahooapis.com/v1/yql?q=';
         // var APP_ID = 'dj0yJmk9aVJUbGlZWUtEbEFlJmQ9WVdrOWNGcGFRek14TldrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD03MQ--';
         // var API_QUERY = 'select * from yahoo.finance.historicaldata where symbol = "YHOO" and startDate = "2009-09-11" and endDate = "2010-03-10"';
-        var symbols = ['GBP', 'AUD','JPY'];
+        var symbols = $scope.searchSymbol.toUpperCase().split(',');
+        var payload = {};
+
+        if ($scope.searchProductType === 'CUR') {
+            payload = {
+                symbols: symbols,
+                minProb: parseFloat($scope.searchMinProbChange),
+                minValChange: parseFloat($scope.searchMinValChange),
+                minPer: parseFloat($scope.searchMinPerChange)
+            };
+        }
+
         $scope.toggleModal();
         $httpshooter.queue({
             // url: BASE_URL + encodeURIComponent(API_QUERY) + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&appid=' + APP_ID,
@@ -27,12 +42,10 @@ nevApp.controller('seasonCtrl', function ($state, $scope, $httpshooter, $localSt
             headers: {
                 'Token': $localStorage.session.token
             },
-            data: {
-                symbols: symbols
-            }
+            data: payload
         }).then(function (data) {
             $scope.currencyData = data.data;
-            console.log($scope.currencyData,"yes");
+            console.log($scope.currencyData, "yes");
         });
     }
 });
