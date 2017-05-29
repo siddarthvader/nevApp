18ee63e8cf43f6853db7cbb62c66cf2c69d13a09
siddharth jain <siddharthjn93@gmail.com>
2017-05-30 02:10:43 +0530

    post changed

--- app/config/var.js
+++ app/config/var.js
@@ -1,6 +1,6 @@
 // var server='http://192.168.1.79:3000/'
 // var server="http://127.0.0.1:3000/";
-var server="http://localhost:3030/"
+var server="http://localhost:3000/"
 // var server="https://enigmatic-savannah-85059.herokuapp.com/";
 var api={
     email:server+'email',
